c4786762c3db3957574e090148fdb92fda318c5a
fguru06 <fhassan06@gmail.com>
2025-05-21 13:32:03 -0400

    gitignore added

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB7TRkIX3WqxPQpjUdKyvtK_CHUW3Osu9k",
-  authDomain: "likert-scale-9d9fc.firebaseapp.com",
-  projectId: "likert-scale-9d9fc",
-  storageBucket: "likert-scale-9d9fc.appspot.com",
-  messagingSenderId: "622362736614",
-  appId: "1:622362736614:web:9c3f482817d4e8aeecc827",
-  measurementId: "G-T3N4YF0ZG9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export { db };
